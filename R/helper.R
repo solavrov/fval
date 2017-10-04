d900b968e00bd32f24f3d5ff938af2f27a1e9210
solavrov <2slavrov@gmail.com>
2017-10-04 16:29:35 +0300

    matrixSwitch() is added to helper

--- R/helper.R
+++ R/helper.R
@@ -1,14 +1,15 @@
 
-#' Common switch that takes vector as an expression argument - helper.R
+#' Common switch that takes expression as vector (helper.R)
 #'
-#' @param expression Expression
-#' @param ... Cases
+#' @param expression Expression vector
+#' @param ... Cases and results
 #'
-#' @return Case that matches expression
+#' @return Result of case that matches expression[i]
 #' @export
 #'
 #' @examples
 #' vectorSwitch(c("a","c"), a = 1, b = 2, c = 3, 4)
+#'
 vectorSwitch <- function(expression, ...) {
 
   result <- numeric()
@@ -20,3 +21,33 @@ vectorSwitch <- function(expression, ...) {
   return (result)
 
 }
+
+
+#' Switch that takes expression and results as vectors (helper.R)
+#'
+#' @param expression Expression vector
+#' @param ... Cases and vectors of results
+#'
+#' @return Result[i] of case that matches expression[i]
+#' @export
+#'
+#' @examples
+#' matrixSwitch(c("a","b","c"), a = 1:3, b = 11:13, c = 101:103, NA)
+#'
+matrixSwitch <- function(expression, ...) {
+
+  result <- numeric()
+
+  for (i in 1:length(expression)) {
+    result[i] <- switch(expression[i], ...)[i]
+  }
+
+  return (result)
+
+}
+
+
+
+
+
+
