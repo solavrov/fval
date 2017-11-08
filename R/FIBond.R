c14a0aaa83abd1d7cb0ccff3b90ee4d95f2e5970
solavrov <2slavrov@gmail.com>
2017-11-08 17:07:22 +0300

    takeCFactor.FIBond() is introduced

--- R/FIBond.R
+++ R/FIBond.R
@@ -133,6 +133,26 @@ print.FIBond <- function(bond) {
 }
 
 
+#' Return cfactor of FIBond object
+#'
+#' @param bond FIBond object (can be a list)
+#'
+#' @return CFactor (can be a vector)
+#' @export
+takeCFactor.FIBond <- function(bond) {
+
+  len <- checkParams(bond)
+
+  cf <- numeric()
+
+  for (i in 1:len) {
+    cf[i] <- e(bond, i)$cfactor
+  }
+
+  return (cf)
+
+}
+
 #' Calculate coupon time i.e. days passed over days in coupon period for FIBond object
 #'
 #' @param bond FIBond object (can be a list)
